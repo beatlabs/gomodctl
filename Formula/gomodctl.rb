5d92b9aec394579d2f2279269f5ceaf630e1a691
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-04 09:55:48 +0000

    Brew formula update for gomodctl version v0.2.1

--- Formula/gomodctl.rb
+++ Formula/gomodctl.rb
@@ -2,16 +2,16 @@
 class Gomodctl < Formula
   desc "search,check and update go modules"
   homepage "https://github.com/beatlabs/gomodctl"
-  version "0.1.2"
+  version "0.2.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/beatlabs/gomodctl/releases/download/v0.1.2/gomodctl_Darwin_x86_64.tar.gz"
-    sha256 "212c189f89c295c9cad95a0c30c284d2a05b4584ff9d3c84de41aecc0cc12ce7"
+    url "https://github.com/beatlabs/gomodctl/releases/download/v0.2.1/gomodctl_Darwin_x86_64.tar.gz"
+    sha256 "af60a2ab36f27117d83d71d1a42a6e2caad0c30bf51f7e551616aea99f74abab"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/beatlabs/gomodctl/releases/download/v0.1.2/gomodctl_Linux_x86_64.tar.gz"
-      sha256 "27fadeb9a4d91f6036f633d93ef9a1edd73a0472fb83bc87995ebfc092085963"
+      url "https://github.com/beatlabs/gomodctl/releases/download/v0.2.1/gomodctl_Linux_x86_64.tar.gz"
+      sha256 "ad942f7158231c2b0ce2e40a60265a8c3bdc5cf818654d972f78d62bab8c558e"
     end
   end
 
